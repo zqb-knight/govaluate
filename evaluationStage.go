bff10f80e34992abf4e85d168860d33a91e23feb
zqb-knight <42669984+zqb-knight@users.noreply.github.com>
2021-12-02 18:45:40 +0800

    feat: 自定义float64比较

--- evaluationStage.go
+++ evaluationStage.go
@@ -135,6 +135,13 @@ func ltStage(left interface{}, right interface{}, parameters Parameters) (interf
 	return boolIface(left.(float64) < right.(float64)), nil
 }
 func equalStage(left interface{}, right interface{}, parameters Parameters) (interface{}, error) {
+	switch left.(type) {
+        case float64:
+                switch right.(type) {
+                case float64:
+                        return math.Abs(left.(float64) - right.(float64)) < 0.01, nil
+                }
+        }
 	return boolIface(reflect.DeepEqual(left, right)), nil
 }
 func notEqualStage(left interface{}, right interface{}, parameters Parameters) (interface{}, error) {
